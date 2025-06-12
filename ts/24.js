7a3d133a5bf0f353ca549b448acf0570297e9a98
Goutam Majumder <majumder.goutam@vit.ac.in>
2025-06-12 21:04:54 +0530

    Add 24.ts and compiled 24.js

---
+++ ts/24.js
@@ -0,0 +1,10 @@
+function CheckOddEven(num) {
+    if (num % 2 == 0) {
+        console.log("".concat(num, " is even"));
+    }
+    else {
+        console.log("".concat(num, " is odd"));
+    }
+}
+CheckOddEven(30);
+CheckOddEven(23);
---
+++ ts/24.ts
@@ -0,0 +1,11 @@
+
+function CheckOddEven(num : number): void{
+    if (num % 2 == 0){
+        console.log(`${num} is even`)
+    }else{
+        console.log(`${num} is odd`)
+    }
+}
+
+CheckOddEven(30)
+CheckOddEven(23)
