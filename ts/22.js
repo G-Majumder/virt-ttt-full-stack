e3d7d90a94dc9ac4e42285823d735bdd3d172c9e
Goutam Majumder <majumder.goutam@vit.ac.in>
2025-06-14 21:04:44 +0530

    Add 22.ts and compiled 22.js

---
+++ ts/22.js
@@ -0,0 +1,6 @@
+function getSum(num1, num2) {
+    return num1 + num2;
+}
+var num1 = 10;
+var num2 = 20;
+console.log("".concat(num1, " + ").concat(num2, " = ").concat(getSum(num1, num2)));
---
+++ ts/22.ts
@@ -0,0 +1,8 @@
+function getSum(num1 : number, num2 : number) : number{
+    return num1 + num2;
+}
+
+let num1 = 10;
+let num2 = 20;
+
+console.log(`${num1} + ${num2} = ${getSum(num1, num2)}`);
