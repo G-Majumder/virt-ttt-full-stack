cf42fdd5478ddbebbf32e5589a666c9997fdc3d5
Goutam Majumder <majumder.goutam@vit.ac.in>
2025-07-11 13:28:26 +0530

    Add App.tsx and Welcome.tsx to proj1 folder

---
+++ react-Ex-1/proj1/App.tsx
@@ -0,0 +1,13 @@
+// App.tsx
+import React from 'react';
+import Welcome from './Welcome';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Welcome name="Goutam" />
+    </div>
+  );
+};
+
+export default App;
---
+++ react-Ex-1/proj1/Welcome.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+interface WelcomeProps{
+    name : string;
+}
+
+
+const Welcome : React.FC<WelcomeProps> = ({ name }) => {
+    return <h1>Welcome {name}.</h1>;
+};
+
+export default Welcome
